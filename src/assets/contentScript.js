3f1f014d2ec0d9637526a201ac7c0ecb41b1f18d
fuji <haya10.88ha@gmail.com>
2020-12-17 01:54:10 +0900

    connet server

--- src/assets/contentScript.js
+++ src/assets/contentScript.js
@@ -1,10 +1,38 @@
-const el = document.querySelector("#rso");
+const ENDPOINT = "http://127.0.0.1:8080/api/filters";
 
-for (const child of el.children) {
-  const e = child.getElementsByClassName("LC20lb DKV0Md")[0];
-  if (!e?.children[0]) {
-    continue;
+const elements = document.getElementsByClassName("yuRUbf");
+
+async function Do() {
+  const urls = [];
+  for (const element of elements) {
+    const url = element.querySelector("a").getAttribute("href");
+    urls.push(url);
   }
-  const badge = Math.random() > 0.3 ? "👍" : "👎";
-  e.children[0].innerHTML = e.innerHTML + " " + badge;
+
+  const response = await fetch(ENDPOINT, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    body: JSON.stringify({ urls: urls }) // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+  });
+  const results = await response.json();
+  console.log(results);
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const e = element.getElementsByClassName("LC20lb DKV0Md")[0];
+    if (!e?.children[0]) {
+      continue;
+    }
+    console.log(results[i]);
+    const badge = parseInt(results[i]) > 0.3 ? "👍" : "👎";
+    e.children[0].innerHTML = e.innerHTML + " " + badge;
+  }
 }
+
+Do();

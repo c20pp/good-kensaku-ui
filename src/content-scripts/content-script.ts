77ad371e92558b7ea41e3a493bbd2d53e3563123
fuji <haya10.88ha@gmail.com>
2021-01-24 18:12:31 +0900

    use content script

--- src/content-scripts/content-script.ts
+++ src/content-scripts/content-script.ts
@@ -1,1 +1,49 @@
-console.log("Hello from the content-script");
+const ENDPOINT = "http://127.0.0.1:4010/filters";
+
+const elements = document.getElementsByClassName("yuRUbf");
+
+async function postFilters(urls: string[]) {
+  const response = await fetch(ENDPOINT, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      "Content-Type": "application/json"
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    body: JSON.stringify({ urls: urls }) // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+  });
+  return response.json();
+}
+
+function addBadge(element: Element, badge: string) {
+  const e = element.getElementsByClassName("LC20lb DKV0Md")[0];
+  if (!e?.children[0]) {
+    return;
+  }
+  e.children[0].innerHTML = badge + e.innerHTML;
+}
+
+function deleteBadge(element: Element) {
+  const e = element.getElementsByClassName("LC20lb DKV0Md")[0];
+  if (!e?.children[0]) {
+    return;
+  }
+  e.children[0].innerHTML = e.innerHTML.slice(8); // delete <span>🤔
+}
+
+Promise.all(
+  Array.prototype.map.call(elements, async (element: Element) => {
+    addBadge(element, "🤔");
+    const url = element.querySelector("a")?.getAttribute("href");
+    let res;
+    if (typeof url === "string") {
+      res = await postFilters([url]);
+    }
+
+    const badge = res.results[0] > 0.5 ? "✅" : "❌";
+    deleteBadge(element);
+    addBadge(element, badge);
+  })
+);
